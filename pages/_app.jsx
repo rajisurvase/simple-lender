ea20d6cda579cc982c47b35e969bf29a3c2f75c1
Gourisankar Sahoo <gourisankar.sahoo@webskitters.com>
2022-08-21 18:15:15 +0530

    dv

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,35 @@
+import { Provider } from 'react-redux';
 import Layout from '../Components/Layout/Layout';
+import { store } from '../Redux/Store';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import '../styles/globals.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+       <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        closeButton={true}
+        toastStyle={{
+          backgroundColor: "black",
+          color: "white",
+        }}
+      />{" "}
     </>
   );
 }
