b908628535adfabb40e691b6b10c70ecbd87fa50
Gourisankar Sahoo <gourisankar.sahoo@webskitters.com>
2022-08-28 20:28:58 +0530

    dv

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,10 +4,16 @@ export default function Document() {
   return (
     <Html>
      <Head>
-     <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/vercel.svg"></link>
-          <meta name="theme-color" content="#fff" />
-
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
         <Main />
