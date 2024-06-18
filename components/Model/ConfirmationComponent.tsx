7a3cc15094044606697de556cdd09df3981cbf1b
piraji <piraji.s@whizit.co.in>
2024-06-18 09:19:57 +0530

    confirmation-model-implemented

--- components/Model/ConfirmationComponent.tsx
+++ components/Model/ConfirmationComponent.tsx
@@ -6,32 +6,37 @@ import {
   Typography,
   IconButton,
   Avatar,
+  Stack,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-  borderRadius: 2,
-  textAlign: 'center',
-};
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   boxShadow: 24,
+//   p: 4,
+//   borderRadius: 2,
+//   textAlign: 'center',
+// };
 
-const ConfirmationComponent = () => {
-  const [open, setOpen] = useState(true); // Modal is open by default
+type ConfirmationComponentProps = {
+  handleConfirmationModel : ()=>void
+}
+
+const ConfirmationComponent = ({handleConfirmationModel}:ConfirmationComponentProps ) => {
+  // const [open, setOpen] = useState(true); // Modal is open by default
 
 
 
   return (
-    <Modal open={open} >
-      <Box sx={style}>
-        <IconButton
+    // <Modal open={open} >
+      <Box>
+        {/* <IconButton
           aria-label="close"
          
           sx={{
@@ -42,18 +47,24 @@ const ConfirmationComponent = () => {
           }}
         >
           <CloseIcon />
-        </IconButton>
-        <Avatar sx={{ bgcolor: 'grey.200', mx: 'auto', mb: 2 }}>
-          <DeleteIcon sx={{ color: 'black' }} />
-        </Avatar>
-        <Typography variant="h6" component="h2" gutterBottom>
+        </IconButton> */}
+        {/* <Avatar sx={{ bgcolor: 'grey.200', mx: 'auto', mb: 2 }}> */}
+        <Stack display="flex" flexDirection="row" justifyContent="center" py={2} >
+        {/* <IconButton> */}
+          <DeleteIcon sx={{ color: 'black',fontSize:"3rem" }}   />
+          {/* </IconButton> */}
+          </Stack>
+        {/* </Avatar> */}
+       <Box textAlign="center" >
+       <Typography variant="h6" component="h2" gutterBottom>
           Are you sure you want to delete this ride
         </Typography>
         <Typography variant="body2" color="text.secondary" gutterBottom>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu eros
           ipsum. Vestibulum ultricies odio.
         </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+       </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center', py: 2 }}>
           <Button
             variant="contained"
             color="error"
@@ -66,12 +77,13 @@ const ConfirmationComponent = () => {
             variant="contained"
             color="secondary"
            // Close the modal when clicked
+           onClick={handleConfirmationModel}
           >
             Cancel
           </Button>
         </Box>
       </Box>
-    </Modal>
+    // </Modal>
   );
 };
 
