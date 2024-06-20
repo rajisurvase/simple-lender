b9dd7ce98252d080b9b230d32f982ff2720d2101
unknown <moteprachi1998@gmail.com>
2024-06-20 23:02:54 +0530

    space added

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -2,7 +2,7 @@ import ResetPasswordComponent from "@/components/Auth/ResetPasswordComponent";
 import React from "react";
 
 const ResetPassword = () => {
-  return <ResetPasswordComponent />;
+  return <ResetPasswordComponent   />;
 };
 
 export default ResetPassword;
